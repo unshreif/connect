bb0d9ba3df69c8ebf6408bbde3e39779622cd741
Muhmd Samy <149331205+unshreif@users.noreply.github.com>
2024-09-02 14:14:18 +0300

    src files

---
+++ index.js
@@ -0,0 +1,29 @@
+let face = location.url = 'https://www.facebook.com/unshreif';
+let insta = location.url = 'https://www.instagram.com/unshreif';
+let linked = location.url = 'https://www.linkedin.com/in/unshreif';
+let x = location.url = 'https://x.com/unshreif';
+let github = location.url = 'https://github.com/unshreif';
+let saraha = location.url = 'https://saraha.com/unshreif';
+let mail = location.url = 'mailto:unshreif@gmail.com';
+
+document.getElementById('FaceBook').addEventListener('click', function() {
+    window.open(face);
+});
+document.getElementById('Instagram').addEventListener('click', function() {
+    window.open(insta);
+});
+document.getElementById('Linkedin').addEventListener('click', function() {
+    window.open(linked);
+});
+document.getElementById('X').addEventListener('click', function() {
+    window.open(x);
+});
+document.getElementById('github').addEventListener('click', function() {
+    window.open(github);
+});
+document.getElementById('sarahah').addEventListener('click', function() {
+    window.open(saraha);
+});
+document.getElementById('mail').addEventListener('click', function() {
+    window.open(mail);
+});
